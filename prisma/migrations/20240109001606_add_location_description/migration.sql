0072605b07e8a187ccf1ff34c3899c0874912cdd
Pedro Santos <P3dro.santosc@gmail.com>
2024-01-08 21:31:01 -0300

    feat: add location description

---
+++ prisma/migrations/20240109001606_add_location_description/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, nisl sed aliquam ultricies, nunc nisl aliquet justo, nec aliquam nisl nunc nec nisl.';
