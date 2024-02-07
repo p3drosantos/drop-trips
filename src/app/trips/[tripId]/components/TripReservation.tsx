f1405417402fbbb0f685db16f4c679beb4245186
Pedro Santos <P3dro.santosc@gmail.com>
2024-02-06 21:31:01 -0300

    style: adapt trip reservation to desktop

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -86,7 +86,10 @@ const TripReservation = ({
   const endDate = watch("endDate");
 
   return (
-    <div className="flex flex-col px-5">
+    <div className="flex flex-col px-5 lg:min-w-[380px] lg:p-5 lg:border-grayLighter lg:border lg:rounded-lg lg:shadow-md">
+      <p className="text-xl hidden text-primaryDarker mb-4 lg:block">
+        <span className="font-semibold">R${pricePerDay}</span> por dia
+      </p>
       <div className="flex gap-4">
         <Controller
           name="startDate"
@@ -162,7 +165,7 @@ const TripReservation = ({
             : "R$0"}
         </p>
       </div>
-      <div className="pb-10 border-b border-grayLighter w-full">
+      <div className="pb-10 border-b border-grayLighter w-full lg:border-none lg:pb-0">
         <Button
           className="mt-3 w-full"
           onClick={() => handleSubmit(onSubmit)()}
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -23,17 +23,23 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
   if (!trip) return null;
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto lg:px-40">
       <TripHeader trip={trip} />
-      <TripReservation
-        tripId={trip.id}
-        tripEndDate={trip.endDate}
-        tripStartDate={trip.startDate}
-        maxGuests={trip.maxGuests}
-        pricePerDay={Number(trip.pricePerDay)}
-      />
-      <TripDescription description={trip.description} />
-      <TripHighlights highlights={trip.highlights} />
+      <div className="flex flex-col lg:flex-row lg:mt-12 lg:gap-20">
+        <div className="lg:order-2">
+          <TripReservation
+            tripId={trip.id}
+            tripEndDate={trip.endDate}
+            tripStartDate={trip.startDate}
+            maxGuests={trip.maxGuests}
+            pricePerDay={Number(trip.pricePerDay)}
+          />
+        </div>
+        <div className="lg:order-1">
+          <TripDescription description={trip.description} />
+          <TripHighlights highlights={trip.highlights} />
+        </div>
+      </div>
       <TripLocation
         location={trip.location}
         locationDescription={trip.locationDescription}
