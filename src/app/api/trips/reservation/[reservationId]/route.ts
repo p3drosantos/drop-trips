552c4a44b18c125f4f20441728b216c2b4d38467
Pedro Santos <P3dro.santosc@gmail.com>
2024-03-05 21:47:49 -0300

    feat: add type from route delete

--- src/app/api/trips/reservation/[reservationId]/route.ts
+++ src/app/api/trips/reservation/[reservationId]/route.ts
@@ -2,16 +2,11 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function DELETE(
-  req: Request,
+  _request: Request,
   { params: { reservationId } }: { params: { reservationId: string } }
-) {
+): Promise<void | Response> {
   if (!reservationId) {
-    return {
-      status: 400,
-      body: {
-        message: "Missing reservation id",
-      },
-    };
+    return new NextResponse("Reservation ID is required", { status: 400 });
   }
 
   const reservation = await prisma.tripReservation.delete({
